9a64a399987996c854ede51343bbc8ead04b8335
Supti <supti.debnath@gmail.com>
2017-02-07 23:13:09 +0600

    A basic variable example.

--- variable-example/variable-example 5.c
+++ variable-example/variable-example 5.c
@@ -1,4 +1,21 @@
 #include<stdio.h>
 
 int main()
-{}
+{
+    int a;
+
+    a = 1000;
+    printf("Value of a is %d\n", a);
+
+    a = 21000;
+    printf("Value of a is %d\n", a);
+
+    a = 1000000;
+    printf("Value of a is %d\n", a);
+
+    a = -100000;
+    printf("Value of a is %d\n", a);
+
+    return 0;
+
+}
