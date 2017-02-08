c6d51e0388c321ac00a02cd42965e44e83a6f1c4
Supti <supti.debnath@gmail.com>
2017-02-08 18:48:23 +0600

    A basic variable example.

---
+++ variable-example/variable-example 8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Enter value of a:");
+    scanf("%d", &a);
+    printf("Enter value of b:");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+}
