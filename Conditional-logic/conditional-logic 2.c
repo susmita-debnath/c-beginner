c3684098330d97e4de3547b01048aaba1c3956bd
Supti <supti.debnath@gmail.com>
2017-02-10 15:43:24 +0600

    Another example of condition.

--- Conditional-logic/conditional-logic 2.c
+++ Conditional-logic/conditional-logic 2.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+    int n;
+    n = 10;
+
+    if(n < 10){
+        printf("The number is negative\n");
+    }
+    else {
+        printf("The number is positive\n");
+    }
+
+    return 0;
+}
