09e6e1b61a570386b8e8e76eb574304d849a9d0f
susmita-debnath <supti.debnath@gmail.com>
2017-04-14 23:19:41 +0600

    Printing words from a sentence by using loop.

---
+++ loop/loop-15.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1002], word[100];
+    int i, j = 0, length, is_word_started;
+
+    gets(s);
+
+    length = strlen(s);
+
+    is_word_started = 0;
+
+    for ( i = 0; i < length; i++ ){
+        if( s[i] >= 'a' && s[i] <= 'z' ){
+            if ( is_word_started == 0 ){
+                is_word_started = 1;
+                word[j] = 'A' + s[i] - 'a'; // first character is capital
+                j++;
+            }
+            else {
+                word[j] = s[i];
+                j++;
+            }
+        }
+        else if ( s[i] >= 'A' && s[i] <= 'Z' ){
+            if ( is_word_started == 0 ){
+                is_word_started = 1;
+            }
+            word[j] = s[i];
+            j++;
+        }
+        else if ( s[i] >= '0' && s[i] <= '9' ){
+            if ( is_word_started == 0 ){
+                is_word_started = 1;
+            }
+            word[j] = s[i];
+            j++;
+        }
+        else {
+            if ( is_word_started == 1 ){
+                is_word_started = 0;
+                word[j] = '\0';
+                printf("%s\n", word);
+                j = 0;
+            }
+        }
+
+        if( i + 1 == length ){
+            if ( is_word_started == 1 ){
+                is_word_started = 0;
+                word[j] = '\0';
+                printf("%s\n", word);
+                j = 0;
+            }
+        }
+    }
+
+    return 0;
+}
