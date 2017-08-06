b185c9d35f2df046af2c950a9f150f12a18b2b2d
Susmita Debnath <supti.debnath@gmail.com>
2017-08-06 22:05:13 +0600

    example of basic input and output of string.

---
+++ string/basic-input-output.c
@@ -0,0 +1,21 @@
+/// String Basic ( Difference Between char array & string )
+
+#include <stdio.h>
+int main()
+{
+    char ch[100];
+    int i;
+    i=0;
+
+    gets(ch);
+    printf("%s", ch);
+
+    //while(ch[i] != '\0'){
+      //  printf("%c", ch[i]);
+        //i++;
+    //}
+
+
+    return 0;
+}
+
