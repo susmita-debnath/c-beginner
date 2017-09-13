d34a95a385af5f6e6bd5d4ae49a470f8be1d3ca2
Susmita Debnath <supti.debnath@gmail.com>
2017-09-13 22:15:44 +0600

    string function.

---
+++ string/string_function.c
@@ -0,0 +1,11 @@
+/// STRING FUNCTION strlen strcopy strcat strcmp
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    ///123456789123456789123456789
+    ///123456789123456789123456789
+
+    return 0;
+}
