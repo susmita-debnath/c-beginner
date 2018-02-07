fb612acf607d7aeffcf6d6888804fc356d36f39c
Susmita Debnath <supti.debnath@gmail.com>
2018-02-07 20:38:13 +0600

    pointer.

---
+++ pointer/closer_3.c
@@ -0,0 +1,19 @@
+/// Pointer closer look
+
+#include <stdio.h>
+
+int main()
+{
+    /*int *p, *q;
+    int x, y;
+    p = &x;
+    x = 5;
+
+    q = &(*(&x));
+    *p = 6;
+    printf("%d\n", *q);*/
+
+    int arr[100];
+
+    return 0;
+}
